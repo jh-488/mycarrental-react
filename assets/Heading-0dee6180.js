58881eca7296702da6fac9315ce6a5cfe704303e
jh-488 <jihadhyadi01@gmail.com>
2023-11-01 14:34:33 +0100

    Updates

---
+++ assets/Heading-0dee6180.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-8d4aef15.js";const i=({title:e,subtitle:r})=>s.jsx(s.Fragment,{children:s.jsxs("div",{className:"heading",children:[s.jsx("h1",{children:e}),s.jsx("p",{children:r})]})});export{i as H};
